67ea0e6678a238835a1ce7dfad93ea390db19aa9
Aryan <aryan.r.t@hotmail.com>
2020-07-11 12:46:37 -0400

    One-vs-All Classifier Training Prediction

--- machine-learning-ex3/ex3/predictOneVsAll.m
+++ machine-learning-ex3/ex3/predictOneVsAll.m
@@ -28,11 +28,10 @@ X = [ones(m, 1) X];
 %       max element, for more information see 'help max'. If your examples 
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
-%       
 
 
-
-
+temp = X * all_theta';
+[temp2,p] = max(temp, [], 2);
 
 
 
